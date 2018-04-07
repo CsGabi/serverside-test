69fda13e69b7be3ef08d6406ce020457a5936311
Cseko, Gabor <gabor.cseko@lhsystems.com>
2018-04-07 22:56:33 +0200

    Generated initial maven project

---
+++ src/main/java/com/sainsburys/serverside-test/App.java
@@ -0,0 +1,13 @@
+package com.sainsburys.serverside-test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
