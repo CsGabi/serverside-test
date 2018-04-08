46b5db794e6e3c90dfafcd0bc41075f9bb5f0b33
Cseko, Gabor <gabor.cseko@lhsystems.com>
2018-04-08 11:42:56 +0200

    Implement first css selectors in order to look for products details from HTML page.

--- src/main/java/com/sainsburys/serverside_test/HTTPFetcher.java
+++ src/main/java/com/sainsburys/serverside_test/HTTPFetcher.java
@@ -1,24 +1,48 @@
 package com.sainsburys.serverside_test;
 
+import java.io.IOException;
 import java.net.URL;
 import org.jsoup.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import javax.json.JsonObject;
 
 public class HTTPFetcher {
   private Connection _connection;
+  private URL _url;
 
   public URL getUrl() {
     return _url;
   }
 
-  public void setUrl(URL _url) {
-    _url = _url;
+  public void setUrl(URL url) {
+    _url = url;
   }
-
-  private URL _url;
 
   public HTTPFetcher(URL url){
     _url = url;
   }
 
+  public JsonObject fetchingData(){
+
+    _connection = Jsoup.connect(_url.toString());
+
+    try {
+      Document document = _connection.get();
+      Elements elements = document.select("#productLister .gridItem");
+
+      for (Element element: elements){
+        String productName = element.select("h3 a").text();
+        String pricePerUnit = element.select(".pricePerUnit").text();
+        String pricePerMeasure = element.select("pricePerMeasure").text();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return null;
+  }
 
 }
--- src/main/java/com/sainsburys/serverside_test/Main.java
+++ src/main/java/com/sainsburys/serverside_test/Main.java
@@ -1,13 +1,21 @@
 package com.sainsburys.serverside_test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Main class
  *
  */
 public class Main {
-  private String url = "";
+  private static String url = "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";
 
   public static void main( String[] args ){
-    System.out.println();
+    try {
+      HTTPFetcher httpFetcher = new HTTPFetcher(new URL(url));
+      httpFetcher.fetchingData();
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+    }
   }
 }
